f9ec7f2f90c4d78cbc96583f8431d4ec6df7c4e2
SirMefju <mateuszcieslik0@gmail.com>
2020-04-19 15:34:37 +0200

    Task 0.0

---
+++ src/com/company/Animal.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Animal
+{
+    final String species;
+    String name;
+    private Double weight = 10.0;
+
+    public Animal(String species)
+    {
+        this.species = species;
+    }
+}
---
+++ src/com/company/Phone.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Phone
+{
+    public final Double screenSize;
+    public final boolean isAndroid;
+
+    public Phone(Double screenSize, boolean isAndroid)
+    {
+        this.screenSize = screenSize;
+        this.isAndroid = isAndroid;
+    }
+}
